24c3150bba3f994024c14c2dab652cba0adbff4d
Balázs <balazs@otakomaiya.com>
2024-11-05 23:18:40 +0100

    New avatar, yearly housekeeping

--- app/page.tsx
+++ app/page.tsx
@@ -17,9 +17,13 @@ export default function Home() {
                 />
                 <Summary>
                     Hey, I’m Balázs. I love building beautiful, user-experience-focused digital experiences. I’ve
-                    Co-Founded a successful Men’s Jewellery Brand called Ezzra, built a popular mobile app used by tens
-                    of thousands called PartyPass, all the whilst working on some exciting projects as a Software
-                    Engineer over at Typeform, Booking.com, PokerStars and more.
+                    Co-Founded a successful Men’s Jewellery Brand called Ezzra all the whilst working on some exciting
+                    projects as a Software Engineer over at Typeform, Booking.com, PokerStars and more.
+
+                    <br/> <br/>
+
+                    Currently building a cross-platform (iOS, Android, Web) a retail investment app at OTP Bank with
+                    Flutter.
 
                     <Links>
                         {/*<Link target='_blank' href=''>Blog</Link>*/}
@@ -41,7 +45,12 @@ export default function Home() {
 
                     Having have fallen in love with <b>React</b> & <b>TypeScript</b>, I’ve been using them to create
                     beautiful, performant
-                    and scalable web applications for the past 5 years.
+                    and scalable web applications for the past 6 years.
+
+                    <br/> <br/>
+
+                    Most recently, I've had the opportunity to deep-dive into building cross-platform apps
+                    with <b>Flutter </b> in a large-scale, enterprise project for the largest bank in Hungary.
                 </Paragraph>
             </Section>
 
@@ -99,99 +108,78 @@ export default function Home() {
                     & Romania.
                 </Paragraph>
             </Section>
-
-            <Section>
-                <Heading>Co-Founder of&nbsp;
-                    <Link
-                        target='_blank'
-                        href='https://apps.apple.com/us/app/partypass/id6450241816'
-                    >
-                        PartyPass
-                    </Link>
-                </Heading>
-                <Paragraph>
-                    PartyPass, launched in 2023, is a mobile app that allows you to plan your night out in
-                    Budapest with complete confidence.
-
-                    <br/> <br/>
-
-                    We&apos;ve partnered with every major nightclub & bar in the city, to provide you with the best,
-                    yet importantly unbiased nightlife guide available – with up to date information on events happening
-                    real-time.
-                </Paragraph>
-            </Section>
         </Container>
     )
 }
 
 const Container = styled.main`
-  max-width: 800px;
-  margin: 0 auto;
+    max-width: 800px;
+    margin: 0 auto;
 
-  @media (max-width: 800px) {
-    width: 100%;
-  }
+    @media (max-width: 800px) {
+        width: 100%;
+    }
 `;
 
 const Name = styled.div`
-  text-transform: uppercase;
-  font-size: 75px;
-  text-align: center;
-  margin-bottom: 24px;
-  font-family: var(--font-telesha);
+    text-transform: uppercase;
+    font-size: 75px;
+    text-align: center;
+    margin-bottom: 24px;
+    font-family: var(--font-telesha);
 
-  @media (max-width: 800px) {
-    font-size: 48px;
-  }
+    @media (max-width: 800px) {
+        font-size: 48px;
+    }
 `
 
 const Header = styled.header`
-  display: flex;
-  gap: 32px;
-  align-items: center;
-  margin-bottom: 48px;
+    display: flex;
+    gap: 32px;
+    align-items: center;
+    margin-bottom: 48px;
 
-  @media (max-width: 800px) {
-    flex-direction: column;
-    text-align: center;
-    gap: 16px;
-  }
+    @media (max-width: 800px) {
+        flex-direction: column;
+        text-align: center;
+        gap: 16px;
+    }
 `
 
 const Portrait = styled(Image)`
-  border-radius: 50%;
+    border-radius: 50%;
 `
 
 const Summary = styled.div`
-  font-size: 16px;
-  line-height: 1.5;
-  color: var(--paragraph-color);
+    font-size: 16px;
+    line-height: 1.5;
+    color: var(--paragraph-color);
 `
 
 const Links = styled.div`
-  display: flex;
-  gap: 16px;
-  margin-top: 16px;
+    display: flex;
+    gap: 16px;
+    margin-top: 16px;
 
-  @media (max-width: 800px) {
-    justify-content: center;
-  }
+    @media (max-width: 800px) {
+        justify-content: center;
+    }
 `
 
 const Section = styled.section`
-  margin-bottom: 48px;
+    margin-bottom: 48px;
 `
 
 const Heading = styled.h2`
-  font-size: 20px;
-  font-weight: 500;
+    font-size: 20px;
+    font-weight: 500;
 `
 
 const Link = styled.a`
-  text-decoration: underline;
-  color: var(--link-color);
+    text-decoration: underline;
+    color: var(--link-color);
 `
 
 const Paragraph = styled.p`
-  color: var(--paragraph-color)
+    color: var(--paragraph-color)
 `
