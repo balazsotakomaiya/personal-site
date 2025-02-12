b8feb172e98d782884251bec06832798757dad2e
Balázs <balazs@otakomaiya.com>
2025-02-12 20:53:13 +0100

    Tailwind V4

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-      <div></div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
   );
 }
