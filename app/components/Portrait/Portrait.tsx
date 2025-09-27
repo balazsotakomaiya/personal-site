4f311f25bfd1bc5e054de306fdd19754de923091
Balázs <balazs@otakomaiya.com>
2025-09-27 11:11:03 +0200

    Grain effect over portrait

--- app/components/Portrait/Portrait.tsx
+++ app/components/Portrait/Portrait.tsx
@@ -5,11 +5,13 @@ import portrait from '@/public/assets/images/portrait.png';
 export const Portrait: React.FC = () => {
     return (
         <div className='relative mr-2 lg:mr-3'>
-            <Image
-                className='relative z-20 rounded-md lg:rounded-none w-full h-full lg:w-128 3xl:w-168'
-                src={portrait}
-                alt='Portrait'
-            />
+            <div className='relative grain-overlay overflow-hidden rounded-md lg:rounded-none'>
+                <Image
+                    className='relative z-20 rounded-md lg:rounded-none w-full h-full lg:w-128 3xl:w-168'
+                    src={portrait}
+                    alt='Portrait'
+                />
+            </div>
             <div
                 className='absolute z-10 bg-custom-gold w-full rounded-md lg:rounded-none lg:w-128 3xl:w-168 h-full top-2 left-2 lg:top-3 lg:left-3'
             />
