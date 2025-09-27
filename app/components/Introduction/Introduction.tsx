0c6dd7b922ce69d846201fa7271bdcb98ada9f60
Balázs <balazs@otakomaiya.com>
2025-09-27 11:23:11 +0200

    Update Introduction.tsx

--- app/components/Introduction/Introduction.tsx
+++ app/components/Introduction/Introduction.tsx
@@ -4,7 +4,7 @@ export const Introduction: React.FC = () => {
     return (
         <div className='flex flex-col text-custom-cream'>
             <h3 className='font-sans text-4xl mb-4'>THE FIRST IMPRESSION</h3>
-            <div className='font-mono text-xs tracking-tight opacity-90'>
+            <div className='font-mono text-xs lg:text-sm tracking-tight opacity-90'>
                 Born in Budapest, I started my first web development agency at 14. By 16, I had joined DEPT, a leading
                 digital agency in Manchester, kickstarting a five-year UK tech adventure before returning to Hungary's
                 capital.
