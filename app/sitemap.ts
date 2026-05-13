2c0ffa39537ba7c1badef5f321686b07dd2f3556
Balázs <balazs@otakomaiya.com>
2026-05-13 21:57:38 +0200

    OG fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import { getBlogPosts } from 'app/blog/utils'
 
-export const baseUrl = 'https://balazs.otakomaiya.com'
+export const baseUrl = 'https://otakomaiya.com'
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
